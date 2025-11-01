1207e456521343bb5a6790a2c0c76cd165cc8dd6
Ian Halstead <ihalstea@iastate.edu>
2025-11-01 07:46:52 -0500

    Created admin dashboard in frontend

--- src/main/client/src/routes/AppRoutes.tsx
+++ src/main/client/src/routes/AppRoutes.tsx
@@ -2,11 +2,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "../pages/login";
 import "../index.css";
 import Register from "../pages/register";
+import AdminDashboard from "../pages/AdminDashboard";
 
 export default function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/admin" element={<AdminDashboard />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
