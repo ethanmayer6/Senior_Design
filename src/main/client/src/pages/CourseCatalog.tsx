d092ffea4de66f319cb82aa3c32757a03a7a8696
Josh Scheitler <joshs1@iastate.edu>
2025-11-02 21:14:53 -0600

    Basic Catalog Setup

--- src/main/client/src/pages/CourseCatalog.tsx
+++ src/main/client/src/pages/CourseCatalog.tsx
@@ -1,10 +1,16 @@
-import { CourseCard } from "../components/CourseCard";
-import type { Course } from "../types/course";
+import {CourseCard} from "../components/CourseCard";
+import type {Course} from "../types/course";
 import axios from "axios";
 import {useEffect, useState} from "react";
+import {Button} from "primereact/button";
+import {InputText} from "primereact/inputtext";
+import {Panel} from "primereact/panel"
+import { TreeSelect } from 'primereact/treeselect';
 
 export default function CourseCatalog() {
-    const[courses, setCourses] = useState<Course[]>([]);
+    const [courses, setCourses] = useState<Course[]>([]);
+    const [nodes, setNodes] = useState(null);
+    const [selectedNodeKeys, setSelectedNodeKeys] = useState(null);
 
     const getCourses = async (): Promise<void> => {
         try {
@@ -21,10 +27,35 @@ export default function CourseCatalog() {
     }, []);
 
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
-            {courses.map((course) => (
-                <CourseCard key={course.courseIdent} course={course} />
-            ))}
+        <div className="min-h-screen p-4 flex gap-4">
+            <aside className="w-1/4">
+                <Panel header="Filters" className="h-full">
+                    <h1>College</h1> {/* COMS, SE */}
+                    <h1>Class Number</h1> {/* 100 Level, 200 Level */}
+                    <h1></h1> {/* COMS, SE */}
+                </Panel>
+            </aside>
+
+            <main className="flex-1 flex flex-col gap-4">
+                <div className="flex flex-row gap-4">
+                    <InputText placeholder="Search" className="w-full bg-gray-700 border border-gray-700"/>
+                    <Button icon="pi pi-sort-alt"/>
+                </div>
+                <div className="flex flex-col gap-4 w-full">
+                    {courses.map((course) => (
+                        <div key={course.courseIdent} className="w-full">
+                            <CourseCard course={course}/>
+                        </div>
+                    ))}
+                </div>
+            </main>
         </div>
     );
-}
+};
+
+
+// <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
+//     {courses.map((course) => (
+//         <CourseCard key={course.courseIdent} course={course} />
+//     ))}
+            // </div>
