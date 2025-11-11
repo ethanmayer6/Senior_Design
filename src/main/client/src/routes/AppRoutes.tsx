986702e2977cb1eecc56f260b9f87a11666440b5
Zach Schmitz <zach24@iastate.edu>
2025-11-10 19:21:16 -0600

    Added Badges

--- src/main/client/src/routes/AppRoutes.tsx
+++ src/main/client/src/routes/AppRoutes.tsx
@@ -4,6 +4,7 @@ import "../index.css";
 import Register from "../pages/register.tsx";
 import AdminDashboard from "../pages/AdminDashboard";
 import CourseCatalog from "../pages/CourseCatalog.tsx";
+import CourseBadges from "../pages/CourseBadges.tsx";
 
 export default function AppRoutes() {
   return (
@@ -13,6 +14,7 @@ export default function AppRoutes() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/catalog" element={<CourseCatalog />} />
+        <Route path="/badges" element={<CourseBadges />} />
       </Routes>
     </BrowserRouter>
   );
